391aef0b6ccf49c06b3929a7eb28eb4a833ac200
Jaidev sah <jaidevsah007@gmail.com>
2022-10-21 07:44:26 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="ns-wrapper">
+        <h1 data-ns-test={'project-name'} >Police App</h1>
+        <h6 data-ns-test={'project-description'} >To help police catch real culprit</h6>
+        <h6>To help police catch real culprit</h6>
+        <h6>To help police catch real culprit</h6>
+    </div>
+  )
+}
+
+
+export default App;
